f2aec26fb452c2ba13e1cfda7d7342720232121d
Øyvind Aukner <22494963+aukoyy@users.noreply.github.com>
2025-02-10 15:10:08 +0100

    upgrade packages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,12 @@
 import { defineConfig } from 'astro/config';
 
 import tailwind from '@astrojs/tailwind';
+import tailwindcss from "@tailwindcss/vite";
+
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
